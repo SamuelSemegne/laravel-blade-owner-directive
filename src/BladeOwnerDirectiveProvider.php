b5e6e802ff391d979b31906f699aa57e671dc61a
Sam <samsemegne@gmail.com>
2018-12-12 11:30:58 +0100

    Adds the provider

---
+++ src/BladeOwnerDirectiveProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SamuelSemegne\LaravelBladeOwnerDirective;
+
+use Gate;
+use Blade;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class BladeOwnerDirectiveProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /**
+         * This directive checks whether every route-binded-model belongs to the current user.
+         */
+        Blade::if('owner', function() {
+            $modelCount = 0;
+            foreach (Request::route()->parameters() as $key => $value) {
+              if ($value instanceof Model) {
+                $modelCount++;
+                // Bail on the first denail.
+                if (Gate::denies('touch-thing', $value)) {
+                    return false;
+                }
+              }
+            }
+
+            // We passed all gates.
+            return ($modelCount ? true : false);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
